6039425643228fee7a894d310fcf1a1fc4845a26
77u4 <github@jh0.eu>
2015-11-26 17:50:46 +0100

    added JS functionality

--- js/custom.js
+++ js/custom.js
@@ -15,26 +15,6 @@ jQuery(window).load(function() {
 
 $(".video-container").fitVids();
 
-
-/* =================================
-===  MAILCHIMP                 ====
-=================================== */
-
-$('.mailchimp').ajaxChimp({
-    callback: mailchimpCallback,
-    url: "http://webdesign7.us6.list-manage.com/subscribe/post?u=9445a2e155b82208d73433060&amp;id=16dc80e353" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".  
-});
-
-function mailchimpCallback(resp) {
-     if (resp.result === 'success') {
-        $('.subscription-success').html('<i class="icon_check_alt2"></i><br/>' + resp.msg).fadeIn(1000);
-        $('.subscription-error').fadeOut(500);
-        
-    } else if(resp.result === 'error') {
-        $('.subscription-error').html('<i class="icon_close_alt2"></i><br/>' + resp.msg).fadeIn(1000);
-    }  
-}
-
 /* =================================
 ===  STICKY NAV                 ====
 =================================== */
@@ -213,34 +193,57 @@ $("#subscribe").submit(function (e) {
 
 
 /* =================================
-===  CONTACT FORM          ====
+===  NEWSLETTER FORM            ====
 =================================== */
-$("#contact").submit(function (e) {
+/* ==================================
+=== EXPAND NEWSLETTER CHECKBOXES ====
+=================================== */
+$("#subscriber-email").bind('keydown', function() {
+        if($("#subscriber-email").val() != ""){
+            $("#themenkategorien").fadeIn(1000);
+            $("#themenkategorien").css("display", "inline");
+        } else {
+            $("#themenkategorien").fadeOut(1000);
+            $("#themenkategorien").css("display", "none");
+            //display: none is set automatically. normally.
+        }
+    }
+);
+
+$("#newsletter-form").submit(function (e) {
     e.preventDefault();
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var subject = $("#subject").val();
-    var message = $("#message").val();
-    var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
+
+    //read email address in the newsletter form
+    var email = $("#subscriber-email").val();
+
+    //initalizes array for checkbox-values
+    var newsletterThemen = new Array();
+
+    //read
+    $('input[name="newsletter-themen"]:checked').each(function() {
+        newsletterThemen.push(this.value);
+    });
+
+    var dataString = 'email=' + email + '&themen=' + newsletterThemen;
 
     function isValidEmail(emailAddress) {
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(emailAddress);
     };
 
-    if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
+    if (isValidEmail(email) && (newsletterThemen.length > 0)) {
         $.ajax({
             type: "POST",
             url: "sendmail.php",
             data: dataString,
             success: function () {
-                $('.success').fadeIn(1000);
-                $('.error').fadeOut(500);
+                $('.subscription-success').fadeIn(1000);
+                $('.subscription-success').fadeOut(5000);
             }
         });
     } else {
-        $('.error').fadeIn(1000);
-        $('.success').fadeOut(500);
+        $('.subscription-error').fadeIn(1000);
+        $('.subscription-error').fadeOut(5000);
     }
 
     return false;
