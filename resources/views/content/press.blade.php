615283ab86244dee23fa456d8482aeb984b1b449
77u4 <github@jh0.eu>
2016-02-08 21:33:04 +0100

    Updated Pressebereich

--- resources/views/content/press.blade.php
+++ resources/views/content/press.blade.php
@@ -8,20 +8,27 @@
 <h3>Material / Digitale Pressemappe</h3>
 <dl class="dl-horizontal">
     <dt>Texte</dt>
-    <dd><a href="https://drive.google.com/uc?export=download&id=0BwSBwiF3Q_NfNVp0X2cyT1l2dG8">Pressemeldung (PDF) &raquo;</a></dd>
+    <dd><a href="https://docs.google.com/uc?id=0B8p7nXAP_GL_MjRIUHdKeVBjVmc&export=download">Pressemitteilung (PDF) &raquo;</a></dd>
+    <dd><a href="https://docs.google.com/uc?id=0B2a8aX_CyNJveGs0QnEyTURnNVE&export=download">Pressemappe Sanktionsfrei (PDF) &raquo;</a></dd>
     <dd><a href="">Hintergrundinformationen Hartz-IV (PDF) &raquo;</a></dd>
     <dd><a href="">Hintergrundinformationen Sanktionsfrei (PDF) &raquo;</a></dd>
     <hr/>
     <dt>O-Töne</dt>
+    <dd style="color: #595959;">Werden nach der Pressekonferenz hochgeladen.</dd>
+    <!--
     <dd><a href="">Inge Hannemann (MP3) &raquo;</a></dd>
     <dd><a href="">Michael Bohmeyer (MP3) &raquo;</a></dd>
     <dd><a href="">Dirk Feiertag (MP3) &raquo;</a></dd>
+    -->
     <hr/>
     <dt>Bilder</dt>
+    <dd style="color: #595959;">Ein Fotoarchiv mit Bildern der Pressekonferenz wird nach ebenjener hochgeladen.</dd>
+    <!--
     <dd><a href="">Inge Hannemann &raquo;</a></dd>
     <dd><a href="">Michael Bohmeyer &raquo;</a></dd>
     <dd><a href="">Fotoarchiv Pressekonferenz &raquo;</a></dd>
+    -->
     <hr/>
     <dt>Video</dt>
-    <dd><a href="">Kampagnenvideo (MP4) &raquo;</a></dd>
+    <dd><a href="https://www.youtube.com/watch?v=goh1tNmKh6A">Kampagnenvideo bei YouTube &raquo;</a></dd>
 </dl>
