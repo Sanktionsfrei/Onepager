d2b2a450e7d108fc458d914cf520e93eba4515d4
ZengineChris <christian@zengine.org>
2016-02-15 20:34:25 +0100

    add faq route and view

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.site')
+
+@section('content')
+    <section class="container">
+        <div class="row">
+            <div class="col-xs-12 content-container">
+                <h1>Ich werde das tolle FAQ</h1>
+            </div>
+        </div>
+    </section>
+@endsection
