aa38b6ccc870504414acc59bcada3353b74f7111
77u4 <github@jh0.eu>
2016-02-16 17:58:50 +0100

    new margin between q's

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -9,33 +9,31 @@
                 	<h1 class="dark-text" style="text-align:center;">Häufig gestellte Fragen (FAQ)</h1>
 				<div class="colored-line"></div>
 
-				<br/><br/><br/>
-
-                <h4 id="1" class="main-color">Was ist Sanktionsfrei? <a href="#1"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="1" class="main-color" style="margin-top:20px;">Was ist Sanktionsfrei? <a href="#1"><i class="icon_link_alt"></i></a></h4>
                 <p>Sanktionsfrei verbindet zwei Sachen zu einem Projekt: sanktionsfrei.de wird eine Plattform, die Betroffene im Umgang mit dem Jobcenter unterstützt und stärkt, gleichzeitig ist Sanktionsfrei eine Kampagne zur Abschaffung der Sanktionen.</p>
                 <hr/>
-                <h4 id="2" class="main-color">Was ist euer Ziel? <a href="#2"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="2" class="main-color" style="margin-top:20px;">Was ist euer Ziel? <a href="#2"><i class="icon_link_alt"></i></a></h4>
                 <p>Wir wollen die Zahl der erfolgreichen Widersprüche und Klagen massiv erhöhen. Um das zu erreichen werden wir Betroffene schnell, kompetent und kostenfrei unterstützen. Letztlich wollen wir damit die Sanktionen abschaffen: 1. politisch, indem die Gesellschaft erkennt, was für ein Unsinn das ist, und 2. ganz konkret, in dem wir Sanktionen bekämpfen und durch einen Sanktionsfonds ausgleichen.</p>
                 <hr/>
 
-                <h4 id="3" class="main-color">Momentan sammelt ihr Geld per Crowdfunding. Wann startet die Plattform? <a href="#3"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="3" class="main-color" style="margin-top:20px;">Momentan sammelt ihr Geld per Crowdfunding. Wann startet die Plattform? <a href="#3"><i class="icon_link_alt"></i></a></h4>
                 <p>Sobald wir die Fundingschwelle von 75.000 € erreicht haben, beginnen wir mit der Programmierung der Plattform. Nach spätestens sechs Monaten - also im Herbst 2016 - werden wir die erste Version von Sanktionsfrei veröffentlichen.</p>
                 <hr/>
-                <h4 id="4" class="main-color">Wohin kann ich mich wenden, wenn ich schon jetzt Stress mit dem Jobcenter habe? <a href="#4"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="4" class="main-color" style="margin-top:20px;">Wohin kann ich mich wenden, wenn ich schon jetzt Stress mit dem Jobcenter habe? <a href="#4"><i class="icon_link_alt"></i></a></h4>
                 <p>Sanktionsfrei befindet sich derzeit noch im Aufbau. Daher können wir dich jetzt leider noch nicht in Auseinandersetzungen mit dem Jobcenter unterstützen. Wende dich bitte mit deinen Fragen z. B. an das Tacheles-Diskussionsforum unter: www.tacheles-sozialhilfe.de. Auch auf <a href="http://www.gegen-hartz.de">gegen-hartz.de</a> sowie <a href="http://altonabloggt.com">altonabloggt.com</a> findest du viele Informationen und Hilfe.<br/>
                 Wenn du eine Beratungsstelle oder einen auf das SGB II/III/XII spezialisierten Rechtsanwalt suchst, kannst du diese über <a href="http://www.my-sozialberatung.de/adressen">das bundesweite Adressverzeichnis</a> finden.</p>
                 <hr/>
-                <h4 id="5" class="main-color">Wann startet der Solidarfonds und wie genau soll er funktionieren? <a href="#5"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="5" class="main-color" style="margin-top:20px;">Wann startet der Solidarfonds und wie genau soll er funktionieren? <a href="#5"><i class="icon_link_alt"></i></a></h4>
                 <p>Das Modell des Solidarfonds werden wir an Hand der Rechtsprechung des Bundessozialgerichts zu den Nothilfezahlugen entwickeln (so z.B: BSG vom 17.06.2010 - B 14 AS 46/09 R). Hier gibt es verschiedene mögliche Varianten. Sobald man über unsere Plattform durch Widersprüche und Klagen gegen Sanktionen vorgehen kann, wollen wir auch mit dem Solidartopf am Start sein.</p>
                 <hr/>
-                <h4 id="6" class="main-color">Sind meine Daten bei Sanktionsfrei sicher? <a href="#6"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="6" class="main-color" style="margin-top:20px;">Sind meine Daten bei Sanktionsfrei sicher? <a href="#6"><i class="icon_link_alt"></i></a></h4>
                 <p>Wir legen bei der Programmierung höchsten Wert auf Datenschutz und sichere Datenübertragung. Sämtliche Daten werden nur verschlüsselt abgelegt und versendet. Wir speichern so wenig wie möglich auf unseren Servern. Das, was wir zwingend speichern müssen, sichern wir nach dem aktuellen Stand der Technik mit modernen Sicherungsverfahren.</p>
                 <hr/>
-                <h4 id="7" class="main-color">Wer steckt hinter Sanktionsfrei? <a href="#7"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="7" class="main-color" style="margin-top:20px;">Wer steckt hinter Sanktionsfrei? <a href="#7"><i class="icon_link_alt"></i></a></h4>
                 <p>Sanktionsfrei wird vom Verein „Sanktionsfrei n.e.V” getragen, der als gemeinnützig anerkannt ist. Die Eintragung ins Registergericht läuft gerade. Neben Inge Hannemann und Michael Bohmeyer gehören 12 weitere engagierte Köpfe zum Sanktionsfrei-Team.</p>
                 <hr/>
 
-                <h4 id="8" class="main-color">Wozu genau braucht ihr die 150.000 EUR, die ihr sammelt? <a href="#8"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="8" class="main-color" style="margin-top:20px;">Wozu genau braucht ihr die 150.000 EUR, die ihr sammelt? <a href="#8"><i class="icon_link_alt"></i></a></h4>
                 <p>Wir wollen mit Sanktionsfrei ein langfristiges und umfangreiches Projekt aufbauen, bei dem Datenschutz und die sichere Datenübermittlung zwischen Betroffenen, Beratungsstellen und dem Jobcenter höchste Priorität hat. Das Geld, das wir mit dem Crowdfunding sammeln, ist dabei für die ersten 8 Monate geplant (6 Monate Entwicklung, 2 Monate Praxiseinsatz und Betreuung der Plattform).<br/>
 
 				Bisher haben insgesamt 12 Menschen das Projekt größtenteils ehrenamtlich voran gebracht. Einige davon werden weiterhin nach ihren Möglichkeiten mit an der Verwirklichung von Sanktionsfrei arbeiten. Wenn die Plattform gut werden soll, müssen einige Menschen aber Vollzeit am Projekt arbeiten. Mit 150.000 € können wir insgesamt acht Menschen acht Monate lang bezahlen. Die größte Gruppe dabei sind fünf Programmierer*innen. Dazu kommen zwei Personen für Social Media, Presse, Öffentlichkeitsarbeit und Projektleitung sowie eine halbe Stelle für die rechtliche Beratung unseres Projekts. Die monatliche Basiskosten des Vereins, wie etwa Büromiete und Bürobedarf, sind vergleichsweise gering.<br/>
@@ -44,7 +42,7 @@
 
 				Sofern wir nur 75.000 € erreichen, können nur halb so viele Menschen am Projekt arbeiten, so dass der Aufbau der Plattform entsprechend länger dauern würde. Auch in diesem Fall bringen wir aber in den ersten sechs Monaten eine erste, kleinere Version der Plattform an den Start.</p>
                 <hr/>
-                <h4 id="9" class="main-color">Wie wird sich Sanktionsfrei darüber hinaus finanzieren? <a href="#9"><i class="icon_link_alt"></i></a></h4>
+                <h4 id="9" class="main-color" style="margin-top:20px;">Wie wird sich Sanktionsfrei darüber hinaus finanzieren? <a href="#9"><i class="icon_link_alt"></i></a></h4>
                 <p>Das Crowdfunding für die Entwicklung der Plattform ist der erste Test, um zu sehen, ob es Unterstützung für die Idee der Solidarität mit Hartz-IV-Leistungsberechtigten gibt. Wenn die Idee die Unterstützung erfährt, die wir uns wünschen und an die wir glauben, werden wir die dauerhafte Entwicklung und Pflege sowie den geplanten Solidarfondsrtopf auf Basis von Spenden und aus den Rückvergütungen von gewonnen Widersprüchen und Klagen finanzieren.</p>
                 <hr/>
             </div>
