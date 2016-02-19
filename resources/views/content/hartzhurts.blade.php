2c6b35b823c9e7ea3704a8c092eaa9a305362576
77u4 <github@jh0.eu>
2016-02-19 23:41:40 +0100

    update #1 von vielen

--- resources/views/content/hartzhurts.blade.php
+++ resources/views/content/hartzhurts.blade.php
@@ -2,3 +2,9 @@
 	<div class="colored-line"></div>
 		<div class="section-description">Teile dein Erlebnis im Jobcenter und lasse die Welt vom Unrecht wissen.</div>
 	<div class="colored-line"></div>
+
+
+	<!-- Card Layout -->
+	<div style="background-color: #1FACB6;">
+
+	</div>
