5385be4694241ecd3db44e9065a92f5fa9953f80
77u4 <github@jh0.eu>
2016-03-26 17:26:42 +0100

    added blades for support-site.

---
+++ resources/views/content/support.blade.php
@@ -0,0 +1,7 @@
+<h2 class="dark-text">Sanktionsfrei unterstützen</h2>
+	<div class="colored-line"></div>
+		<div class="section-description">Lorem ipsum dolor sit amet!</div>
+	<div class="colored-line"></div>
+
+
+	<p>Content.</p>
