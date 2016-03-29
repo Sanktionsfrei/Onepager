7fbf812190a6b5c00e7425e2fdd6f981371f2580
johannesponader <johannesponader@googlemail.com>
2016-03-29 18:08:27 +0200

    Update localDonations.php

--- resources/assets/localDonations.php
+++ resources/assets/localDonations.php
@@ -2,8 +2,8 @@
   $localDonations = 1200.00;
 
   
-  #$donationTarget = 150000.00;
-  #$localPercentage = round($localDonations/$donationTarget*100);  
+  $donationTarget = 150000.00;
+  $localPercentage = round($localDonations/$donationTarget*100);  
 
 ?>
 
