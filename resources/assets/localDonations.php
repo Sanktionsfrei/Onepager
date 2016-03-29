41206ef94529baa390e2d82c2f639a9cd3d77091
johannesponader <johannesponader@googlemail.com>
2016-03-29 19:08:42 +0200

    Update localDonations.php

--- resources/assets/localDonations.php
+++ resources/assets/localDonations.php
@@ -1,11 +1,4 @@
 <?php
   $localDonations = 1200.00;
-
-  
-  $donationTarget = 150000.00;
-  $localPercentage = round($localDonations/$donationTarget*100);  
-
-  $localDonationsS = str_replace('.', ',', $localDonations) . ' €';
-
 ?>
 
