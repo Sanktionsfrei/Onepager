1d0da3f2716307df23119368043a179a5926ef03
johannesponader <johannesponader@googlemail.com>
2016-03-29 22:58:25 +0200

    Hotfix hardcoded 1%

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,11 +53,17 @@
                              style="width:{{ $percent }}%">
                              <span class="funding-text">{{ $progressText }}</span>
                         </div>
+                        <!-- fixer Wert-->
+                        <div class="progress-bar progress-bar-warning funding-bar" role="progressbar"
+                             aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"
+                             style="width:1%">
+                             <!--span class="funding-text">{{ $progressText }}</span-->
+                        <!-- variabler Wert-->
                         <!--div class="progress-bar progress-bar-warning funding-bar" role="progressbar"
-                             aria-valuenow="{{ $percent }}" aria-valuemin="0" aria-valuemax="100"
-                             style="width:{{ $percent }}%">
-                             <span class="funding-text">{{ $progressText }}</span>
-                        </div-->
+                             aria-valuenow="{{ $localDonationsPercent }}" aria-valuemin="0" aria-valuemax="100"
+                             style="width:{{ $localDonationsPercent }}%"-->
+                             <!--span class="funding-text">{{ $progressText }}</span-->
+                        </div>
                     </div>
 
                     {{-- BUTTON --}}
