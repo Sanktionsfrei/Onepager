53bc72b11793e37a618fcfb4c89f12792d9dc38b
ZengineChris <christian@zengine.org>
2016-03-30 14:30:55 +0200

    recompile assets

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,6 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
 
 /* =================================
  LOADER
@@ -390,66 +389,9 @@ function notWorkingYet() {
 }
 $(document).ready(function () {
     "use strict";
-    var Form = $('#form');
-    var Button = $('#form-button');
-    var Success = $('#success');
-    var Error = $('#error');
-    var Message = $('#error span');
 
-    Form.submit(function (event) {
 
-        Error.fadeOut(200);
 
-        event.preventDefault();
-
-        var data = Form.serializeArray();
-
-        var subscription = subscribe(data);
-
-        subscription.fail(handleError);
-        subscription.done(handleSuccess)
-
-    });
-
-    function subscribe(data) {
-        return $.ajax({
-            method: 'POST',
-            url: '/subscribe',
-            data: data
-        })
-    }
-
-    function handleSuccess(response) {
-        console.info(response);
-        Button.fadeOut(400, function () {
-            Success.fadeIn(400)
-        })
-
-    }
-
-    function handleError(error) {
-
-        var messages = JSON.parse(error.responseText);
-
-        Message.text(messages.email);
-        Error.fadeIn(400);
-    }
-
-    $("#btnSave").click(function() {
-        alert('jup');
-        html2canvas($("#img2share"), {
-            onrendered: function(canvas) {
-                theCanvas = canvas;
-                document.body.appendChild(canvas);
-
-                // Convert and download as image
-                Canvas2Image.saveAsPNG(canvas);
-                $("#img-out").append(canvas);
-                // Clean up
-                //document.body.removeChild(canvas);
-            }
-        });
-    });
 
 });
 //# sourceMappingURL=main.js.map
