c72d2f487f63eb8d6d24fe21174e6ea0393e3751
SuskaSF <suska@sanktionsfrei.de>
2016-09-19 11:24:13 +0200

    {SW} changed image in components

--- resources/views/components/social-meta.blade.php
+++ resources/views/components/social-meta.blade.php
@@ -1,7 +1,7 @@
 <!-- Schema.org markup for Google+ -->
 <meta itemprop="name" content="Sanktionsfrei">
 <meta itemprop="description" content="Die deutschen Jobcenter dürfen ihre Hilfen unter das Existenzminimum kürzen. Das ist nicht okay. Wir machen deshalb Hartz IV sanktionsfrei.">
-<meta itemprop="image" content="{{ url('images/bg-1.jpg') }}">
+<meta itemprop="image" content="{{ url('images/holding-hands.png') }}">
 
 <!-- Twitter Card data -->
 <meta name="twitter:card" content="summary_large_image">
@@ -10,13 +10,13 @@
 <meta name="twitter:description" content="Die deutschen Jobcenter dürfen ihre Hilfen unter das Existenzminimum kürzen. Das ist nicht okay. Wir machen deshalb Hartz IV sanktionsfrei.">
 <meta name="twitter:creator" content="@sanktionsfrei">
 <!-- Twitter summary card with large image must be at least 280x150px -->
-<meta name="twitter:image:src" content="{{ url('images/bg-1.jpg') }}">
+<meta name="twitter:image:src" content="{{ url('images/holding-hands.png') }}">
 
 <!-- Open Graph data -->
 <meta property="og:title" content="Sanktionsfrei" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{ url('/') }}" />
-<meta property="og:image" content="{{ url('images/bg-1.jpg') }}" />
+<meta property="og:image" content="{{ url('images/holding-hands.png') }}" />
 <meta property="og:description" content="Die deutschen Jobcenter dürfen ihre Hilfen unter das Existenzminimum kürzen. Das ist nicht okay. Wir machen deshalb Hartz IV sanktionsfrei.">
 <meta property="og:site_name" content="Sanktionsfrei" />
 <meta property="fb:app_id" content="1297764360250524" />
